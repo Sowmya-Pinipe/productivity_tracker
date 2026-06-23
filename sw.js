78f7cc542057d91737312f1c7a0984b2a0cab685
Sowmya <sowmyapinipe25@gmail.com>
2026-06-23 22:42:24 +0530

    Fix service worker cache update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "flowspace-v1";
+const CACHE_NAME = "flowspace-v2";
 
 const FILES_TO_CACHE = [
   "/",
@@ -10,18 +10,37 @@ const FILES_TO_CACHE = [
   "/icons/icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+
+  self.skipWaiting();
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
